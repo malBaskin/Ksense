c57084f038200b6995451a395a988ec4e62a10f1
Mallory Baskin <mallorybaskinjr@gmail.com>
2021-11-12 14:15:47 -0500

    first push
    Just basic templates

---
+++ index.js
@@ -0,0 +1,12 @@
+import {FetchWrapper} from "./fetch-wrapper.js";
+
+const api = new FetchWrapper('https://jsonplaceholder.typicode.com/');
+
+const table = document.querySelector("#user-table");
+
+table.addEventListener("onload", () => {
+	api.get("users").then(data =>{
+		console.log(data);
+		//table.textContent = data;
+	})
+})
