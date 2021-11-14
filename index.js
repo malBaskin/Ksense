26d7a59b648007f4a258915c183a871e83b74f44
Mallory Baskin <mallorybaskinjr@gmail.com>
2021-11-14 10:07:39 -0500

    another attempt

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const api = new FetchWrapper('https://jsonplaceholder.typicode.com/');
 
 const table = document.querySelector("#user-table");
 
-table.addEventListener("onload", () => {
+document.addEventListener("onload", () => {
 	api.get("users").then(data =>{
 		console.log(data);
 		//table.textContent = data;
