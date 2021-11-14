d0653fb0a9bcc3291b2b66677a9f91622f39239e
Mallory Baskin <mallorybaskinjr@gmail.com>
2021-11-14 12:20:48 -0500

    working on fetching posts

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const api = new FetchWrapper('https://jsonplaceholder.typicode.com/');
 
 const output = document.querySelector("#user-table");
 const postsOutput= document.querySelector("#popup-list");
+const userID;
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -11,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		 //output.innerHTML = '';
 		data.forEach(user => {
 			console.log(user.name);
-			
+			userID[] = user.ID;
 			try{
 
         		output.insertAdjacentHTML("beforeend",`<td class="user-data"> <button onclick="openMenu()">${user.name}</button></td>`)
@@ -26,16 +27,16 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 
-// function getPosts(){
-//  	api.get("posts").then(data =>{
-//  		data.forEach(posts => {
-//  			try{
+function getPosts(){
+ 	api.get("posts").then(data =>{
+ 		data.forEach(posts => {
+ 			try{
 
-//         		postsOutput.insertAdjacentHTML("beforeend",`<li><div class="card">[${posts.category}] ${posts.title}</div></li>`)
-//     		}
-//     		catch(error){
-//        			 console.error(error)
-//     		}
-//  		})
-//  	})
-//  }
+        		postsOutput.insertAdjacentHTML("beforeend",`<li><div class="card">[${posts.category}] ${posts.title}</div></li>`)
+    		}
+    		catch(error){
+       			 console.error(error)
+    		}
+ 		})
+ 	})
+ }
