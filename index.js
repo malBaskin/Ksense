5f50958373daeba20ae6695afa91bba727390110
Mallory Baskin <mallorybaskinjr@gmail.com>
2021-11-14 10:59:53 -0500

    display names

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		 //output.innerHTML = '';
 		data.forEach(user => {
 			console.log(user.name);
-			//output.insertAdjacentHTML("beforeend",`<td>${user.name}</td>`)
+			output.insertAdjacentHTML("beforeend",`<td>${user.name}</td>`)
 		})
 		//table.textContent = data;
 	})
